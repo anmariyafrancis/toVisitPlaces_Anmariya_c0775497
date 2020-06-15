88d320db649838f02de2b7215c460c544fe191f5
anmariyafrancis <anmariya1915@gmail.com>
2020-06-15 19:36:58 -0400

    [ANMARIYA] created new swift class file

---
+++ toVisitPlaces_Anmariya_c0775497/FavouritePlaces.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritePlaces.swift
+//  toVisitPlaces_Anmariya_c0775497
+//
+//  Created by user176498 on 6/15/20.
+//  Copyright © 2020 user176498. All rights reserved.
+//
+
+class FavouritePlaces{
+    
+    var placeName:String
+    var country:String
+    var postalCode:String
+    var latitude:Double
+    var longitude:Double
+    
+    init(placeName:String,country:String,postalCode:String,latitude:Double,longitude:Double) {
+    
+    }
+}
