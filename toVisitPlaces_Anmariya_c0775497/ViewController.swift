6aae36b7e9eca506acd3734cfa7e72f034e73311
anmariyafrancis <anmariya1915@gmail.com>
2020-06-15 14:26:32 -0400

    Initial Commit

---
+++ toVisitPlaces_Anmariya_c0775497/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_Anmariya_c0775497
+//
+//  Created by user176498 on 6/15/20.
+//  Copyright © 2020 user176498. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
